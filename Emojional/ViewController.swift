d162f4d445bcdaab3b19f4457f051d1513a41283
Julianne <julianneychen@gmail.com>
2021-05-11 17:47:38 -0700

    completed iteration 1

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let emojis = [
+           "😭" : "crying",
+           "😅" : "awkward"
+       ]
+    
+    let custommessages = [
+        "crying" : "don't live in the past! we are here and ",
+        "awkward" : "no one will remember this event! you got this queen"
+        ]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController()
+        
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        
+        present(alertController, animated: true, completion: nil)
+    
     }
 
 
