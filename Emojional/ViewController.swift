ed9a18cbd7bc855cd242da738a93bf90b05f895b
Julianne <julianneychen@gmail.com>
2021-05-11 20:46:10 -0700

    completed iteration 3

--- Emojional/ViewController.swift
+++ Emojional/ViewController.swift
@@ -15,11 +15,10 @@ class ViewController: UIViewController {
            "😅" : "awkward"
        ]
     
-    let custommessages = [
-        "crying" : "don't live in the past! we are here and ",
-        "awkward" : "no one will remember this event! you got this queen"
+    let customMessages = [
+        "crying" : ["don't live in the past!", "tomorrow will be better", "here's a hug"],
+        "awkward" : ["no one will remember this" , "you got this queen", "shake it off" ]
         ]
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -28,14 +27,18 @@ class ViewController: UIViewController {
     
     @IBAction func showMessage(sender: UIButton) {
         
-        let selectedEmotion = sender.titleLabel?.text
+        
+        let selectedEmotion = (sender.titleLabel?.text)!
+        let messageList = customMessages[emojis[selectedEmotion]!]!
+        let num = Int.random(in: 0...2)
+        let emojiMessage = messageList[num]
 
-        let emojiMessage = custommessages[emojis[selectedEmotion!]!]
+//        let emojiMessage = customMessages[emojis[selectedEmotion!]!]
         
         let alertController = UIAlertController()
 
         
-        alertController.addAction(UIAlertAction(title: emojiMessage, style: UIAlertAction.Style.default, handler: nil))
+alertController.addAction(UIAlertAction(title: emojiMessage, style: UIAlertAction.Style.default, handler: nil))
         
         present(alertController, animated: true, completion: nil)
     
